3892ec48860d6d5f7c80a27b3534dcbfb8f7c01f
yaya-bb <2286834433@qq.com>
2022-08-03 16:45:41 +0800

    初始化

---
+++ src/pages/home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nav-header></nav-header>
+  </div>
+</template>
+<script>
+import NavHeader from '@/components/NavHeader.vue'
+export default {
+  name:"home"
+}
+</script>
