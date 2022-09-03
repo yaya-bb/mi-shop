fc26f5cf942df3a5bc212527c327de8e25c848ac
yaya-bb <2286834433@qq.com>
2022-09-03 12:33:05 +0800

    地址和商品数据加载

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -75,6 +75,14 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader',
+        include: [resolve('src/icons')],
+        options: {
+          symbolId: 'icon-[name]' // name代表图标的名字
+        }
       }
     ]
   },
