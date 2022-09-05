edc82d0d9ef35fca615cc11a4e9e2ea9cb4752c6
yaya-bb <2286834433@qq.com>
2022-09-05 09:45:43 +0800

    路由按需加载方案1:resolve => require()

--- src/router/index.js
+++ src/router/index.js
@@ -2,27 +2,33 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home.vue'
 import Index from '@/pages/index.vue'
-import Product from '@/pages/product.vue'
-import Detail from '@/pages/detail.vue'
+// import Product from '@/pages/product.vue'
+// import Detail from '@/pages/detail.vue'
 import Cart from '@/pages/cart.vue'
-import Login from '@/pages/login.vue'
+// import Login from '@/pages/login.vue'
 import Order from '@/pages/order.vue'
 import OrderConfirm from '@/pages/orderConfirm.vue'
 import OrderList from '@/pages/orderList.vue'
 import OrderPay from '@/pages/orderPay.vue'
 import alipay from '@/pages/alipay.vue'
+// import { resolve } from 'core-js/fn/promise'
 
 Vue.use(Router)
 
+// 前端路由按需加载（只拉取需要的），目的防止首屏加载过慢，出现白屏
+// 路由懒加载
 export default new Router({
   routes: [
     {
       path: '/',
       name: 'home',
+      // 默认加载首页，所以不需要做按需加载，从其他页面开始
       component: Home,
       redirect: '/index',
+      // 这样的方法会一开始把所有文件都加载出来，效率低
       children: [
         {
+          // 重定向到首页
           path: '/index',
           name: 'index',
           component: Index
@@ -30,19 +36,23 @@ export default new Router({
         {
           path: '/product/:id',
           name: 'product',
-          component: Product
+          // resolve是ES6中Promise语法，当成功的时候会得到一个resolve对象，抛出去就认为你是成功的了
+          // 通过require去加载页面的组件，里面用resolve把参数抛出去
+          // 这样可以实现产品站的按需加载
+          // 这是一个箭头函数，当访问产品站的时候，会变相的通过箭头函数require把它加载进来
+          component: resolve => require(['@/pages/index.vue'], resolve)
         },
         {
           path: '/detail/:id',
           name: 'detail',
-          component: Detail
+          component: resolve => require(['@/pages/detail.vue'], resolve)
         }
       ]
     },
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: resolve => require(['@/pages/login.vue'], resolve)
     },
     {
       path: '/cart',
